6d70528a41f7411bb7fff3d811e8475da13986cc
LEON <LEON7394@GMAIL.COM>
2025-02-11 12:00:33 +0330

    Custom HTML form combined with Django form

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField(label='نام و نام خانوادگی', max_length=300, required=True, error_messages={
-        'required' : 'لطفا نام و نام خانوادگی خود را وارد نمایید'
-    })
-    email = forms.EmailField(label='ایمیل')
-    subject = forms.CharField(label= 'موضوع')
-    messages = forms.CharField(widget=forms.Textarea, label='پیغام شما')
+    full_name = forms.CharField(
+        label='نام و نام خانوادگی',
+        max_length=300,
+        required=True,
+        error_messages={'required': 'لطفا نام و نام خانوادگی خود را وارد نمایید'}
+    )
+    email = forms.EmailField(label='ایمیل', required=True, error_messages={'required': 'ایمیل الزامی است'})
+    subject = forms.CharField(label='موضوع', required=True, error_messages={'required': 'لطفا موضوع را وارد کنید'})
+    messages = forms.CharField(widget=forms.Textarea, label='پیغام شما', required=True, error_messages={'required': 'لطفا پیام خود را وارد کنید'})
--- contact_module/views.py
+++ contact_module/views.py
@@ -3,13 +3,12 @@ from django.urls import reverse
 from .forms import ContactUsForm
 
 def contact_us_page(request):
+    contact_form = ContactUsForm(request.POST or None)
+
     if request.method == "POST":
-        contact_form = ContactUsForm(request.POST)
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
             return redirect('home_page')
-    else:
-        contact_form = ContactUsForm()
 
     return render(request, 'contact_module/contact_page.html', {
         'contact_form': contact_form
