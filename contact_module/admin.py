33b115921281dbf291604cdaeb421cfef70f961a
LEON <LEON7394@GMAIL.COM>
2025-03-08 18:38:56 +0330

    Add Shamsi DateTime to the admin contactUs Forms

--- contact_module/admin.py
+++ contact_module/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from . import models
 
+
 class ContactFunction(admin.ModelAdmin):
-    list_display = ['full_name', 'created_date']
-    fields = ('full_name', 'email', 'title', 'message', 'created_date', 'response', 'is_read_by_admin')
-    readonly_fields = ('created_date',)
+    list_display = ['full_name', 'get_shamsi_date']
+    fields = ('full_name', 'email', 'title', 'message', 'created_date', 'get_shamsi_date', 'response', 'is_read_by_admin')
+    readonly_fields = ('created_date','get_shamsi_date')
+
+    def get_shamsi_date(self, obj):
+        return obj.get_shamsi_date()
+
+    get_shamsi_date.short_description = "تاریخ شمسی"
 
 admin.site.register(models.ContactUs, ContactFunction)
--- contact_module/models.py
+++ contact_module/models.py
@@ -1,17 +1,38 @@
 from django.db import models
+import jdatetime
+import pytz
+
 
 class ContactUs(models.Model):
     full_name = models.CharField(max_length=300, verbose_name='نام و نام خانوادگی')
     email = models.EmailField(max_length=300, verbose_name='ایمیل')
     title = models.CharField(max_length=300, verbose_name='عنوان')
-    message = models.TextField(verbose_name= 'متن تماس با ما')
+    message = models.TextField(verbose_name='متن تماس با ما')
     created_date = models.DateTimeField(verbose_name='تاریخ ایجاد', auto_now_add=True)
-    response = models.TextField(verbose_name= 'متن پاسخ تماس با ما', null=True, blank=True)
+    response = models.TextField(verbose_name='متن پاسخ تماس با ما', null=True, blank=True)
     is_read_by_admin = models.BooleanField(verbose_name='خوانده شده توسط ادمین', default=False)
 
     class Meta:
         verbose_name = 'تماس با ما'
         verbose_name_plural = 'لیست تماس با ما'
+
+    def get_shamsi_date(self):
+        tehran_tz = pytz.timezone('Asia/Tehran')
+        tehran_time = self.created_date.astimezone(tehran_tz)
+
+        shamsi_date = jdatetime.datetime.fromgregorian(datetime=tehran_time)
+
+        persian_months = {
+            "Farvardin": "فروردین", "Ordibehesht": "اردیبهشت", "Khordad": "خرداد",
+            "Tir": "تیر", "Mordad": "مرداد", "Shahrivar": "شهریور",
+            "Mehr": "مهر", "Aban": "آبان", "Azar": "آذر",
+            "Dey": "دی", "Bahman": "بهمن", "Esfand": "اسفند"
+        }
+
+        month_fa = persian_months[shamsi_date.strftime('%B')]
+        return shamsi_date.strftime(f'%d {month_fa} %Y - %H:%M')
+
+    get_shamsi_date.short_description = "تاریخ شمسی"
 
     def __str__(self):
         return self.title
