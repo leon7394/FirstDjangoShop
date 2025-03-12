5c19ec133d322f54079c62a3969026708bc8f9fc
LEON <LEON7394@GMAIL.COM>
2025-03-12 16:14:21 +0330

    add Gitignore and ignore some files

--- contact_module/admin.py
+++ contact_module/admin.py
@@ -3,13 +3,16 @@ from . import models
 
 
 class ContactFunction(admin.ModelAdmin):
-    list_display = ['full_name', 'get_shamsi_date']
-    fields = ('full_name', 'email', 'title', 'message', 'created_date', 'get_shamsi_date', 'response', 'is_read_by_admin')
-    readonly_fields = ('created_date','get_shamsi_date')
 
     def get_shamsi_date(self, obj):
         return obj.get_shamsi_date()
 
     get_shamsi_date.short_description = "تاریخ شمسی"
+
+    list_display = ['full_name', 'get_shamsi_date']
+    fields = ('full_name', 'email', 'title', 'message', 'created_date', 'get_shamsi_date', 'response', 'is_read_by_admin')
+    readonly_fields = ('created_date','get_shamsi_date')
+
+
 
 admin.site.register(models.ContactUs, ContactFunction)
