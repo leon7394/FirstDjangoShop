871e1647961828fb3bff85e5a72ee9e9a1441064
leon <leon7394@gmail.com>
2025-05-13 22:56:04 +0330

    Filter articles by category

--- article_module/views.py
+++ article_module/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
-from article_module.models import Article
-
+from article_module.models import Article, ArticleCategory
 
 
 # class ArticlesView(View):
@@ -10,8 +9,21 @@ from article_module.models import Article
 #         articles = Article.objects.filter(is_active=True)
 #         return render(request, 'article_module/articles_page.html', {'articles': articles})
 
+
 class ArticlesListView(ListView):
     model = Article
     paginate_by = 5
     template_name = 'article_module/articles_page.html'
     context_object_name = 'articles'
+
+    def get_queryset(self):
+        query = super(ArticlesListView, self).get_queryset()
+        category_name = self.kwargs.get('category')
+        if category_name:
+            query = query.filter(selected_categories__url_title__iexact=category_name)
+        return query
+
+
+def article_categories_component(request):
+    article_main_categories = ArticleCategory.objects.filter(is_active=True, parent_id=None)
+    return render(request, 'article_module/components/article_categories_component.html', {'main_categories': article_main_categories})
