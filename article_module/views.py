ddecb90379977b2d14d28576fb4383e9cd82e403
leon <leon7394@gmail.com>
2025-05-15 22:06:56 +0330

    article detail page

--- article_module/views.py
+++ article_module/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from article_module.models import Article, ArticleCategory
-
-
-# class ArticlesView(View):
-#     def get(self, request):
-#         articles = Article.objects.filter(is_active=True)
-#         return render(request, 'article_module/articles_page.html', {'articles': articles})
 
 
 class ArticlesListView(ListView):
     model = Article
-    paginate_by = 5
+    paginate_by = 4
     template_name = 'article_module/articles_page.html'
     context_object_name = 'articles'
 
     def get_queryset(self):
         query = super(ArticlesListView, self).get_queryset()
+        query = query.filter(is_active=True)
         category_name = self.kwargs.get('category')
         if category_name:
             query = query.filter(selected_categories__url_title__iexact=category_name)
         return query
 
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_module/article_detail_page.html'
+
+    def get_queryset(self):
+        query = super(ArticleDetailView, self).get_queryset()
+        query = query.filter(is_active=True)
+        return query
 
 def article_categories_component(request):
     article_main_categories = ArticleCategory.objects.filter(is_active=True, parent_id=None)
