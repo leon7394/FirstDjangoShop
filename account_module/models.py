410770b5c7a76ff85bc93f51c7fad809001b7b0a
leon <leon7394@gmail.com>
2025-06-04 22:04:20 +0330

    create a form for edit profile information in user panel

--- account_module/models.py
+++ account_module/models.py
@@ -5,6 +5,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='images/avatars', null=True, blank=True, verbose_name='تصویر پروفایل')
     email_active_code = models.CharField(max_length=100, unique=True, verbose_name = 'کد فعال سازی ایمیل')
     about_user = models.TextField(null=True, blank=True, verbose_name='درباره شخص')
+    address = models.TextField(null=True, blank=True, verbose_name='آدرس')
 
     class Meta:
         verbose_name = 'کاربر'
--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.generic import TemplateView
+from user_panel_module.forms import EditProfileModelForm
 
 
 class UserPanelDashboardPage(TemplateView):
@@ -12,3 +12,18 @@ class UserPanelDashboardPage(TemplateView):
         if not request.user.is_authenticated:
             return redirect(reverse('login_page'))
         return super().dispatch(request, *args, **kwargs)
+
+class EditUserProfilePage(View):
+    def get(self, request):
+        edit_form = EditProfileModelForm()
+        context = {
+            'form' : edit_form
+        }
+        return render(request, 'user_panel_module/edit_profile_page.html', context)
+
+    def post(self, request):
+        return render(request, 'user_panel_module/edit_profile_page.html', {})
+
+
+def user_panel_menu_component(request):
+    return render(request, "user_panel_module/components/user_panel_menu_component.html")
